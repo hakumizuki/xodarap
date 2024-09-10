f4a6e5c9542bd4718ed7803e62bc66363aff020c
hakumizuki <montanha.masu536@gmail.com>
2024-09-10 20:57:37 +0900

    check

--- src/fun.ts
+++ src/fun.ts
@@ -1,3 +1,9 @@
 export const fun = () => {
-	console.log("Hello via Bun!");
+  console.log("Hello via Bun!");
 };
+
+const main = async () => {
+  console.log("Starting Bun...");
+};
+
+main();
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { fun } from "./fun";
 console.log("Starting Bun...");
 
 await new Promise((r) =>
-	setTimeout(() => {
-		fun();
-		r(void 0);
-	}, 1000),
+  setTimeout(() => {
+    fun();
+    r(void 0);
+  }, 1000),
 );
