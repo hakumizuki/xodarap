bc95215d3de43ac6566b9328ef596754037b64c1
hakumizuki <montanha.masu536@gmail.com>
2024-09-10 20:40:50 +0900

    check

--- src/fun.ts
+++ src/fun.ts
@@ -1,4 +1,3 @@
 export const fun = () => {
-
-  console.log("Hello via Bun!");
-}
+	console.log("Hello via Bun!");
+};
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { fun } from "./fun";
 
 console.log("Starting Bun...");
 
-await new Promise(r => setTimeout(() => {
-  fun();
-  r(void 0);
-}, 1000));
+await new Promise((r) =>
+	setTimeout(() => {
+		fun();
+		r(void 0);
+	}, 1000),
+);
