6255f2c9104b0109dda5206aea905668d5ce4fe1
hakumizuki <montanha.masu536@gmail.com>
2024-09-15 12:22:13 +0900

    rm unnecessary code

--- src/fun.ts
+++
@@ -1,9 +0,0 @@
-export const fun = () => {
-  console.log("Hello via Bun!");
-};
-
-const main = async () => {
-  console.log("Starting Bun...");
-};
-
-main();
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import { fun } from "./fun";
-
 console.log("Starting Bun...");
-
-await new Promise((r) =>
-  setTimeout(() => {
-    fun();
-    r(void 0);
-  }, 1000),
-);
