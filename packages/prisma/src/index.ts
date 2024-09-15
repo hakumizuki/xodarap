e6639ea5b89aed9b44de0c3e2567aa80c2d0d525
hakumizuki <montanha.masu536@gmail.com>
2024-09-15 17:20:50 +0900

    add: add prisma package and setup prisma

---
+++ packages/prisma/src/index.ts
@@ -0,0 +1,3 @@
+export { Prisma, PrismaClient, type PrismaPromise } from "./generated/client";
+export * from "./generated/zod";
+export { PrismaManager, type PrismaClientOptions } from "./prisma-manager";
