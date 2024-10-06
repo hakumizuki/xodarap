55f8753c456641cadc7b92dd74df4e46c464146d
hakumizuki <montanha.masu536@gmail.com>
2024-10-07 02:05:19 +0900

    add: house feature

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PaymentModule } from "./payment/payment.module";
+import { HouseModule } from './house/house.module';
 
 @Module({
-  imports: [PaymentModule],
+  imports: [PaymentModule, HouseModule],
   controllers: [AppController],
   providers: [AppService],
 })
