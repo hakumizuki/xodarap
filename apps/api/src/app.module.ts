e5ce762d0e7ca1a69f08fb8fb754f2e1bdc5cc8e
hakumizuki <montanha.masu536@gmail.com>
2024-10-07 04:06:52 +0900

    clean

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PaymentModule } from "./payment/payment.module";
-import { HouseModule } from './house/house.module';
 
 @Module({
-  imports: [PaymentModule, HouseModule],
+  imports: [PaymentModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
