535451f6a50ce54e2c8895c517beaeec8720fc28
hakumizuki <montanha.masu536@gmail.com>
2025-03-25 22:35:37 +0900

    support: sample controller

--- apps/api/src/sample/sample.controller.ts
+++ apps/api/src/sample/sample.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, HttpException, Post } from "@nestjs/common";
+import { SampleDto } from "./dto/SampleDto";
 import { SampleService } from "./sample.service";
 
 @Controller("sample")
 export class SampleController {
   constructor(private readonly sampleService: SampleService) {}
 
-  @Post()
-  async sample() {
-    return this.sampleService.sample();
+  @Post("sample")
+  async sample(@Body() body: SampleDto) {
+    if (!body.id) {
+      throw new HttpException("id is required", 400);
+    }
+
+    return this.sampleService.execute();
   }
 }
--- apps/api/src/sample/sample.service.ts
+++ apps/api/src/sample/sample.service.ts
@@ -2,10 +2,5 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class SampleService {
-  async sample() {
-    return {
-      message: "Sample endpoint is working!",
-      timestamp: new Date().toISOString(),
-    };
-  }
+  async execute() {}
 }
